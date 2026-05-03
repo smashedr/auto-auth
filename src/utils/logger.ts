c79bfcb84f1e9434bf022e3fc005b9239d74b406
Shane <6071159+smashedr@users.noreply.github.com>
2026-05-03 13:05:36 -0700

    Update logger.ts

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,1 +1,6 @@
-export const debug = import.meta.env.DEV ? console.log.bind(console) : () => {}
+// export const debug = import.meta.env.DEV ? console.log.bind(console) : () => {}
+
+// NOTE: To prepend [debug] and preserver formatting the 1st argument must be a string
+export const debug = import.meta.env.DEV
+  ? (first: string, ...rest: any) => console.log('[debug] ' + first, ...rest)
+  : () => {}
